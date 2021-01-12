06e9d4a4c40633014dcada6d2f7c4cfa938f97cc
James <james.jasenia@gmail.com>
2021-01-13 08:58:53 +1100

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationjetpack/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.bottomnavigationjetpack;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+    SettingsFragment settingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        settingsFragment = new SettingsFragment();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        fragmentTransaction.replace(R.id.flFragment, homeFragment);
+                        fragmentTransaction.commit();
+                        break;
+                    case R.id.settings:
+                        fragmentTransaction.replace(R.id.flFragment, settingsFragment);
+                        fragmentTransaction.commit();
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+}
